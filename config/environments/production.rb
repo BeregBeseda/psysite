00f4f70b6abfb983d380c8f253c279581cdbe570
VitalinaLiane <vitalina.liane@gmail.com>
2014-01-28 14:48:43 +0200

    walk1

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,7 +89,6 @@ Psysite::Application.configure do
   domain:                 'example.com',  
   password:               'sh19781968199719gmail',  
   authentication:         :plain   
-  tls:                    true 
   }  
 
   config.action_mailer.default_url_options = { :host => 'psysite.herokuapp.com' }
