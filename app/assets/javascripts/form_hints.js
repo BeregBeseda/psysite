9373c2da6d141b89043a2f03e8003264c1a3f3fb
VitalinaLiane <vitalina.liane@gmail.com>
2014-01-29 13:25:04 +0200

    hints social icons

--- app/assets/javascripts/form_hints.js
+++ app/assets/javascripts/form_hints.js
@@ -1,26 +1,14 @@
 $(document).ready(function(){
-    $(".input-address").click(function(){
-        $("#top_field").text("Заботимся о безопасности");
-    });
-
-    $(".input-name").click(function(){
-        $("#top_field").text("Как к Вам обращаться");
-    });
-
-    $(".input-message").click(function(){
-        $("#top_field").text("Message");
-    });
-
     $(".input-address").focus(function(){
-        $("#top_field").text("Заботимся о безопасности");
+        $("#top_field").text(encodeURIComponent("Ваша персональная информация будет находиться в безопасности"));
     });
 
     $(".input-name").focus(function(){
-        $("#top_field").text("Как к Вам обращаться");
+        $("#top_field").text(encodeURIComponent("Как к Вам обращаться"));
     });
 
     $(".input-message").focus(function(){
-        $("#top_field").text("Message");
+        $("#top_field").text(encodeURIComponent("Если желаете, опишите в нескольких фразах, какую проблему Вы хотите решить"));
     });
 });
 
