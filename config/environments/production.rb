30221417cbd8c513fd2481aa2ef60449adb6c3d0
VitalinaLiane <vitalina.liane@gmail.com>
2014-06-27 21:11:16 +0300

    production start001

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,7 +4,7 @@ Psysite::Application.configure do
   # Code is not reloaded between requests.
 
 
-  config.cache_classes = true
+  config.cache_classes = false
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both thread web servers
@@ -29,7 +29,7 @@ Psysite::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
