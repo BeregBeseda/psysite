4edcb4811a8c6f6aa69f6a6af20615ca856633de
VitalinaLiane <vitalina.liane@gmail.com>
2014-07-18 22:07:05 +0300

    main block padding - media request

--- app/controllers/products_pays_controller.rb
+++ app/controllers/products_pays_controller.rb
@@ -2,9 +2,12 @@
 class ProductsPaysController < ApplicationController
 
   def index
-    @products_pay_email = params[:email]
-    @products_pay_product_name = params[:product_name]
+    @product = Product.find_by_id(params[:product_id])
+    @user = NewsEmail.find_by_id(params[:user_id])
     $top_field_text = 'Заполните все поля, и, после проверки платежа, в течении суток, на Ваш E-mail придет ссылка на скачивание видеозаписи семинара'
+  end
+
+  def fail
   end
 
 end
