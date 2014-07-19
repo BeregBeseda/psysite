e219b4b0e7bb39e7111fd0b666a8c4a4a80413eb
VitalinaLiane <vitalina.liane@gmail.com>
2014-07-19 13:02:59 +0300

    remove deleted files, fond img blur

--- app/models/example.rb
+++
@@ -1,5 +0,0 @@
-#!/bin/env ruby
-# encoding: utf-8
-class Example < ActiveRecord::Base
-  validates :title, presence: { message: 'Поля "E-mail" и "Имя" обязательны к заполнению' }
-end
--- db/migrate/20140614095923_remove_some_from_products_posts.rb
+++
@@ -1,6 +0,0 @@
-class RemoveSomeFromProductsPosts < ActiveRecord::Migration
-  def change
-    remove_column :products, :short, :string
-    remove_column :posts, :short, :string
-  end
-end
