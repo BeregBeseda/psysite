a9270a6f30c266dd80c0854b850921b3a1bba701
VitalinaLiane <vitalina.liane@gmail.com>
2015-06-20 11:40:50 +0300

    activeadmin new user1

--- db/migrate/20150620113800_devise_create_admin_users.rb
+++ db/migrate/20150620113800_devise_create_admin_users.rb
@@ -1,7 +1,7 @@
 class DeviseCreateAdminUsers < ActiveRecord::Migration
   def migrate(direction)
     super
-    # Create a new user
+    # Create a new user1
     AdminUser.create!(:email => 'bereg.beseda@gmail.com', :password => 'password', :password_confirmation => 'password') if direction == :up
   end
 
