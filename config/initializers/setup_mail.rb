c413743958463faddfe0ff904f97b50f1095f991
VitalinaLiane <vitalina.liane@gmail.com>
2015-06-26 22:36:20 +0300

    add real gmail mailing data (config/initializers/setup_mail.rb)

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -3,7 +3,7 @@ ActionMailer::Base.smtp_settings = {
   :port                 => 587,  
   :domain               => "psysite.herokuapp.com",  
   :user_name            => "bereg.beseda",  
-  :password             => "sh19781968199719",  
+  :password             => "gsh1978196819971",  
   :authentication       => "plain",  
   :enable_starttls_auto => true  
 }  
